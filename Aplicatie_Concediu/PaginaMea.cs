546790d56da4fd0c4300a97ed9a93ff03748b714
Alberto Chirita <alberto.chirita@totalsoft.ro>
2022-08-31 10:44:56 +0300

    a

--- Aplicatie_Concediu/PaginaMea.cs
+++ Aplicatie_Concediu/PaginaMea.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -21,11 +22,39 @@ namespace Aplicatie_Concediu
 
         private void PaginaMea_Load(object sender, EventArgs e)
         {
-            SqlConnection connection = new SqlConnection(@"Data Source = ts2112\SQLEXPRESS; Initial Catalog = BreakingBread; Persist Security Info = True; User ID = internship2022; Password = int");
+            SqlConnection connection = new SqlConnection(@"Data Source = ts2112\SQLEXPRESS; Initial Catalog = BreakingBread; Persist Security Info = True; User ID = internship2022; Password = int ");
             connection.Open();
 
-            SqlCommand command = new SqlCommand("select * from Angajat where id = " + Autentificare.UserId);
-            SqlDataReader dr = command.ExecuteReader();
+            string query = "select * from Angajat where id = '" + Program.UserId + "'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                int id = Convert.ToInt32(reader["id"]);
+                string nume = Convert.ToString(reader["nume"]);
+                string prenume = Convert.ToString(reader["prenume"]);
+                string email = Convert.ToString(reader["email"]);
+                string parola = Convert.ToString(reader["parola"]);
+                DateTime dataAngajare = Convert.ToDateTime(reader["dataAngajare"]);
+                DateTime dataNasterii = Convert.ToDateTime(reader["dataNasterii"]);
+                string cnp = Convert.ToString(reader["cnp"]);
+                string serie = Convert.ToString(reader["serie"]);
+                string no = Convert.ToString(reader["no"]);
+                string nrTelefon = Convert.ToString(reader["nrTelefon"]);
+
+                labelNumeUtilizator.Text = nume + " " + prenume;
+                textBoxNume.Text = nume;
+                textBoxPrenume.Text = prenume;
+                textBoxCnp.Text = cnp;
+                textBoxSerie.Text = serie;
+                textBoxNr.Text = no;
+                textBoxTelefon.Text = nrTelefon;
+                textBoxEmail.Text = email;
+                dateTimePickerDataNasterii.Value = dataNasterii;
+                dateTimePickerDataAngajarii.Value = dataAngajare;
+            }
         }
 
         private void buttonEditeazaProfil_Click(object sender, EventArgs e)
