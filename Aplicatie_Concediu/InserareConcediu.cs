d1eab4b10a43ff1a76fb1d53357715c782e52eac
Gunal Sadic <gunal.sadic@totalsoft.ro>
2022-09-12 11:03:25 +0300

    zileSarbatoare

--- Aplicatie_Concediu/InserareConcediu.cs
+++ Aplicatie_Concediu/InserareConcediu.cs
@@ -152,7 +152,7 @@ namespace Aplicatie_Concediu
         private void buttonIesire_Click(object sender, EventArgs e)
         {
             SesiuneLogIn.angajatLogat = null;
-            this.Close();
+            Application.Exit();
         }
 
         // Click Utilizator Logat
---
+++ Aplicatie_Concediu/Models/ZileLibereAnCurent.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Concediu_WebApi.Models
+{
+    public class ZileLibereAnCurent
+    {
+    
+       public string name { get; set; }
+      
+        public List<Date> date { get; set; }
+
+    }
+}
