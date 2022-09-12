3c06f644cc386b0daee22fee74615b76b8062e3c
Alberto Chirita <alberto.chirita@totalsoft.ro>
2022-09-12 11:44:52 +0300

    UI

--- Aplicatie_Concediu/Autentificare.Designer.cs
+++ Aplicatie_Concediu/Autentificare.Designer.cs
@@ -57,7 +57,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(160)))), ((int)(((byte)(111)))));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(184, 0);
+            this.label1.Location = new System.Drawing.Point(185, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(179, 31);
@@ -72,17 +72,18 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.SetColumnSpan(this.mail, 2);
             this.mail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mail.Location = new System.Drawing.Point(184, 125);
+            this.mail.Location = new System.Drawing.Point(185, 125);
             this.mail.Margin = new System.Windows.Forms.Padding(0, 0, 50, 0);
             this.mail.Multiline = true;
             this.mail.Name = "mail";
-            this.mail.Size = new System.Drawing.Size(596, 30);
+            this.mail.Size = new System.Drawing.Size(598, 30);
             this.mail.TabIndex = 5;
             this.mail.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.mail_KeyPress);
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(133)))), ((int)(((byte)(78)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -91,7 +92,7 @@
             this.button1.Location = new System.Drawing.Point(237, 730);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(596, 69);
+            this.button1.Size = new System.Drawing.Size(596, 79);
             this.button1.TabIndex = 7;
             this.button1.Text = "Autentifica-te";
             this.button1.UseVisualStyleBackColor = false;
@@ -102,7 +103,7 @@
             this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(160)))), ((int)(((byte)(111)))));
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(122, 127);
+            this.pictureBox2.Location = new System.Drawing.Point(123, 127);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(59, 30);
@@ -115,7 +116,7 @@
             this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(160)))), ((int)(((byte)(111)))));
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(122, 227);
+            this.pictureBox3.Location = new System.Drawing.Point(123, 227);
             this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(59, 30);
@@ -133,7 +134,7 @@
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.ForeColor = System.Drawing.Color.Peru;
             this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.linkLabel1.Location = new System.Drawing.Point(184, 275);
+            this.linkLabel1.Location = new System.Drawing.Point(185, 301);
             this.linkLabel1.Margin = new System.Windows.Forms.Padding(0);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(176, 25);
@@ -146,7 +147,7 @@
             // 
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(160)))), ((int)(((byte)(111)))));
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(187, 75);
+            this.label2.Location = new System.Drawing.Point(188, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 50);
             this.label2.TabIndex = 11;
@@ -157,7 +158,7 @@
             // 
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(160)))), ((int)(((byte)(111)))));
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(184, 175);
+            this.label3.Location = new System.Drawing.Point(185, 175);
             this.label3.Margin = new System.Windows.Forms.Padding(0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 50);
@@ -175,10 +176,10 @@
             this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel2.ForeColor = System.Drawing.Color.Peru;
             this.linkLabel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.linkLabel2.Location = new System.Drawing.Point(507, 275);
+            this.linkLabel2.Location = new System.Drawing.Point(509, 301);
             this.linkLabel2.Margin = new System.Windows.Forms.Padding(0);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(323, 25);
+            this.linkLabel2.Size = new System.Drawing.Size(202, 25);
             this.linkLabel2.TabIndex = 13;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "> Creeaza cont nou";
@@ -188,7 +189,7 @@
             // 
             this.pbCaps.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(160)))), ((int)(((byte)(111)))));
             this.pbCaps.Image = ((System.Drawing.Image)(resources.GetObject("pbCaps.Image")));
-            this.pbCaps.Location = new System.Drawing.Point(834, 229);
+            this.pbCaps.Location = new System.Drawing.Point(837, 229);
             this.pbCaps.Margin = new System.Windows.Forms.Padding(4);
             this.pbCaps.Name = "pbCaps";
             this.pbCaps.Size = new System.Drawing.Size(59, 30);
@@ -198,30 +199,33 @@
             // 
             // pbLogoInreg
             // 
-            this.pbLogoInreg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.pbLogoInreg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pbLogoInreg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pbLogoInreg.Image = ((System.Drawing.Image)(resources.GetObject("pbLogoInreg.Image")));
-            this.pbLogoInreg.Location = new System.Drawing.Point(53, 50);
+            this.pbLogoInreg.Location = new System.Drawing.Point(50, 50);
             this.pbLogoInreg.Margin = new System.Windows.Forms.Padding(0, 0, 0, 25);
             this.pbLogoInreg.Name = "pbLogoInreg";
-            this.pbLogoInreg.Size = new System.Drawing.Size(923, 252);
+            this.pbLogoInreg.Size = new System.Drawing.Size(926, 262);
             this.pbLogoInreg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbLogoInreg.TabIndex = 15;
             this.pbLogoInreg.TabStop = false;
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(160)))), ((int)(((byte)(111)))));
             this.panel1.Controls.Add(this.buttonIesire);
             this.panel1.Controls.Add(this.tableLayoutPanel1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.pbLogoInreg);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(10, 10);
             this.panel1.Name = "panel1";
             this.panel1.Padding = new System.Windows.Forms.Padding(50);
-            this.panel1.Size = new System.Drawing.Size(1026, 864);
+            this.panel1.Size = new System.Drawing.Size(1026, 880);
             this.panel1.TabIndex = 16;
             // 
             // buttonIesire
@@ -241,10 +245,10 @@
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.AutoScroll = true;
-            this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.ColumnCount = 4;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
@@ -260,7 +264,7 @@
             this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.pbCaps, 3, 4);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(53, 327);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(50, 327);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 75);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
@@ -270,7 +274,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(923, 326);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(926, 336);
             this.tableLayoutPanel1.TabIndex = 16;
             // 
             // tbParola
@@ -279,12 +283,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.SetColumnSpan(this.tbParola, 2);
             this.tbParola.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbParola.Location = new System.Drawing.Point(184, 225);
+            this.tbParola.Location = new System.Drawing.Point(185, 225);
             this.tbParola.Margin = new System.Windows.Forms.Padding(0, 0, 50, 0);
             this.tbParola.Multiline = true;
             this.tbParola.Name = "tbParola";
             this.tbParola.PasswordChar = '*';
-            this.tbParola.Size = new System.Drawing.Size(596, 30);
+            this.tbParola.Size = new System.Drawing.Size(598, 30);
             this.tbParola.TabIndex = 6;
             this.tbParola.Click += new System.EventHandler(this.tbParola_Click);
             this.tbParola.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbParola_KeyDown);
@@ -295,7 +299,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(133)))), ((int)(((byte)(78)))));
-            this.ClientSize = new System.Drawing.Size(1046, 884);
+            this.ClientSize = new System.Drawing.Size(1046, 900);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -308,7 +312,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbCaps)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogoInreg)).EndInit();
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
